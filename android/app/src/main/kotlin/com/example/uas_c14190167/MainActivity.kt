2585828c4c8364f7f65037022ab49fe08136936c
mchlkevin <you@example.com>
2022-06-23 13:36:41 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/uas_c14190167/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas_c14190167
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MaterialApp(
+    title: "UAS-C14190167",
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("UAS-C14190167"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Column(children: [
+          Text("Test"),
+        ]),
+      ),
+    );
+  }
+}
